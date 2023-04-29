44bf708664ebf18be94f98b9aae9be83b6af458d
Ranga <abhi6equj5@gmail.com>
2023-04-29 18:28:38 +0530

    no lazy

--- build/static/js/468.79989001.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkuniverse=self.webpackChunkuniverse||[]).push([[468],{468:function(e,n,r){r.r(n);r(791);var u=r(184);n.default=function(){return(0,u.jsx)("div",{children:(0,u.jsx)("h1",{children:"error component..."})})}}}]);
-//# sourceMappingURL=468.79989001.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { lazy } from "react";
 import './App.css';
 
-// import Home from "./components/home";
-// import Galary from "./components/galary";
-// import Error from "./components/error";
+import Home from "./components/home";
+import Galary from "./components/galary";
+import Error from "./components/error";
  import Nav from "./components/nav"
 
-const Home = lazy(()=> import("./components/home"))
-const Galary = lazy(()=> import("./components/galary"))
-const Error = lazy(()=> import("./components/error"))
+// const Home = lazy(()=> import("./components/home"))
+// const Galary = lazy(()=> import("./components/galary"))
+// const Error = lazy(()=> import("./components/error"))
 
 function App() {
   return (
